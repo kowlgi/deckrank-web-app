be8afaeef22f41342b3f165eb498ceab032c65dd
Hareesh Nagarajan <hareesh.nagarajan@gmail.com>
2015-06-03 22:11:41 -0700

    move favicon.png

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 // setup express and the environment
 app.set('port', process.env.PORT || 3000);
-app.use(favicon(path.join(__dirname, 'static', 'images', 'favicon.png')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({
